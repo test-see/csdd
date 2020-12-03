08c537e17b4889dc870f441a5ba489f10b05c3f8
wancheo <wang406776@126.com>
2020-12-03 12:16:52 +0800

    done

--- csdd/Middlewares/ExceptionMiddleware.cs
+++ csdd/Middlewares/ExceptionMiddleware.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using foundation.exception;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace csdd.Middlewares
@@ -27,11 +29,9 @@ namespace csdd.Middlewares
             {
                 _logger.LogError(ex, $"Path: {httpContext.Request.Path}. Message: {ex.Message}");
                 httpContext.Response.ContentType = "application/json";
-                await httpContext.Response.WriteAsync(JsonConvert.SerializeObject(new
-                {
-                    StatusCode = httpContext.Response.StatusCode,
-                    Message = ex.Message
-                }));
+                var status = ex is DefaultException ? HttpStatusCode.BadRequest : HttpStatusCode.InternalServerError;
+                httpContext.Response.StatusCode = (int)status;
+                await httpContext.Response.WriteAsync(JsonConvert.SerializeObject(new { Status = status, Message = ex.Message }));
             }
         }
     }
