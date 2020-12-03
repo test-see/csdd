c9d3dd773b6b8c959bffbbbf6c3093ee7b0072ca
wancheo <wang406776@126.com>
2020-12-03 12:43:54 +0800

    done

--- src/domain/user/UserLoginContext.cs
+++ src/domain/user/UserLoginContext.cs
@@ -1,7 +1,7 @@
-﻿using irespository.user;
+﻿using foundation.ef5.poco;
+using foundation.exception;
+using irespository.user;
 using System.Threading.Tasks;
-using System;
-using foundation.ef5.poco;
 
 namespace domain.user
 {
@@ -21,18 +21,18 @@ namespace domain.user
         public User LoginOrRegister(LoginOrRegisterApiModel login)
         {
             if (!_userVerificationCodeRespository.CheckVerificationCode(login))
-                throw new UnauthorizedAccessException("phone or code is invalid.");
+                throw new DefaultException("phone or code is invalid.");
             var user = _userRespository.GetByPhone(login.Phone);
             if (user == null)
-                throw new UnauthorizedAccessException("the user isnot exist.");
+                throw new DefaultException("the user isnot exist.");
             return user;
         }
         public async Task<string> GenerateVerificationCodeAsync(string phone)
         {
             if (!_dataWhitePhoneRespository.Exists(phone))
-                throw new UnauthorizedAccessException("this phone is unauth.");
+                throw new DefaultException("this phone is unauth.");
             if (_userVerificationCodeRespository.GetCountVerificationCodeInMinuteOne(phone) > 0)
-                throw new InvalidOperationException("get the verification code too more.");
+                throw new DefaultException("get the verification code too more.");
             await _userVerificationCodeRespository.InActiveVerificationCodeListAsync(phone);
             return await _userVerificationCodeRespository.AddVerificationCodeAsync(phone);
         }
