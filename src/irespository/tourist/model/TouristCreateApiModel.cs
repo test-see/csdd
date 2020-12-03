7e278d340eb29ab54ce52dadd98f5c1e2106087a
wancheo <wang406776@126.com>
2020-12-03 17:20:42 +0800

    done

--- src/irespository/tourist/model/TouristCreateApiModel.cs
+++ src/irespository/tourist/model/TouristCreateApiModel.cs
@@ -2,5 +2,10 @@
 {
     public class TouristCreateApiModel
     {
+        public string Username { get; set; }
+        public string Phone { get; set; }
+        public int ProvinceId { get; set; }
+        public int IdentityCategoryId { get; set; }
+
     }
 }
