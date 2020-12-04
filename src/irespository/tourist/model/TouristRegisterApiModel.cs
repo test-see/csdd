c85c67a309c13d54325df6d4f893780150a6840b
wancheo <wang406776@126.com>
2020-12-04 18:45:29 +0800

    done

--- src/irespository/tourist/model/TouristRegisterApiModel.cs
+++ src/irespository/tourist/model/TouristRegisterApiModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace irespository.tourist.model
 {
-    public class TouristRegisterApiModel
+    public class TouristRegisterStepOneApiModel
     {
         public string Username { get; set; }
         public string Phone { get; set; }
