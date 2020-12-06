e317baa0ca2082b934f13d012cbf803729e99091
wancheo <wang406776@126.com>
2020-12-06 14:49:49 +0800

    Update OkMessage.cs

--- src/foundation/config/OkMessage.cs
+++ src/foundation/config/OkMessage.cs
@@ -5,9 +5,9 @@ namespace foundation.config
     public class OkMessage
     {
         public dynamic Data { get; } 
-        public int Code { get; } = (int)HttpStatusCode.OK;
-        public string Msg { get; } = string.Empty;
-        public OkMessage(dynamic data) { Data = data; }
+        public int Code { get; } 
+        public string Msg { get; }
+        public OkMessage(dynamic data) { Data = data; Code = (int)HttpStatusCode.OK; Msg = string.Empty;}
         public OkMessage(int code, string msg) { Code = code; Msg = msg; Data = new { }; }
     }
 }
