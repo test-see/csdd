d420620e06daae4efd40b11341a2ef0423503294
wancheo <wang406776@126.com>
2020-12-11 14:29:17 +0800

    clean code

--- src/domain/user/UserLoginContext.cs
+++ src/domain/user/UserLoginContext.cs
@@ -21,20 +21,20 @@ namespace domain.user
         public User Login(LoginApiModel login)
         {
             if (!_userVerificationCodeRespository.CheckVerificationCode(login))
-                throw new DefaultException("phone or code is invalid.");
+                throw new DefaultException("电话号码 或者 验证码不正确.");
             var user = _userRespository.GetByPhone(login.Phone);
             if (user == null)
-                throw new DefaultException("the user isnot exist.");
+                throw new DefaultException("用户不存在.");
             if (user.IsActive == 0)
-                throw new DefaultException("the user isnot active.");
+                throw new DefaultException("该用户已被冻结, 请联系管理员激活.");
             return user;
         }
         public async Task<string> GenerateVerificationCodeAsync(string phone)
         {
             if (!_dataWhitePhoneRespository.Exists(phone))
-                throw new DefaultException("this phone is unauth.");
+                throw new DefaultException("该电话未被授权使用.");
             if (_userVerificationCodeRespository.GetCountVerificationCodeInMinuteOne(phone) > 0)
-                throw new DefaultException("get the verification code too more.");
+                throw new DefaultException("您获取验证码的频率太高了,请稍后再获取.");
             await _userVerificationCodeRespository.InActiveVerificationCodeListAsync(phone);
             return await _userVerificationCodeRespository.AddVerificationCodeAsync(phone);
         }
