76aebebb62764fc3897c942446c7b2a7f13eba84
wancheo <wang406776@126.com>
2020-12-25 16:58:53 +0800

    done

---
+++ src/apitest/User/UserApiTest.cs
@@ -0,0 +1,25 @@
+﻿using apitest.Shared;
+using Flurl;
+using Flurl.Http;
+using foundation.config;
+using irespository.user.model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace apitest.User
+{
+    [TestClass]
+    public class UserApiTest : BaseApiTest
+    {
+        [TestMethod]
+        public async Task User_Post_ReturnListAsync()
+        {
+            var message = await _rootpath
+                .AppendPathSegment("/api/User/list")
+                .WithOAuthBearerToken(await getToken())
+                .PostJsonAsync(new PagerQuery<UserListQueryModel> { })
+                .ReceiveJson<OkMessage<PagerResult<foundation.ef5.poco.User>>>();
+            Assert.AreEqual(200, message.Code);
+        }
+    }
+}
