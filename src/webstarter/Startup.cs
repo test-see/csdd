cef03c003452863daee5582113fed95bb0e09a13
wancheo <wang406776@126.com>
2021-01-04 15:40:33 +0800

    22

--- src/webstarter/Startup.cs
+++ src/webstarter/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
