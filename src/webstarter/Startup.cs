266ec2bb51f68c03300c08792835dfe7c02542d6
wancheo <wang406776@126.com>
2021-01-04 15:55:20 +0800

    test

--- src/webstarter/Startup.cs
+++ src/webstarter/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -80,7 +81,6 @@ namespace csdd
             {
                 options.AddPolicy("RequireAdministratorRole", policy =>
                 {
-                    policy.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
                     policy.RequireRole(AuthorizeRole.Admin.ToString());
                 });
             });
