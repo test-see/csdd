54837deba759b39eb7d0a1c4e53d5f30e0603a87
wancheo <wang406776@126.com>
2021-01-11 19:57:27 +0800

    add test

---
+++ src/apitest/sys/HospitalDepartmentApiTest.cs
@@ -0,0 +1,61 @@
+﻿using apitest.shared;
+using Flurl;
+using Flurl.Http;
+using foundation.config;
+using irespository.hospital.department.model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace apitest.sys
+{
+    [TestClass]
+    public class HospitalDepartmentApiTest : BaseApiTest
+    {
+        [TestMethod]
+        public async Task HospitalDepartment_Post_ReturnListAsync()
+        {
+            var message = await _rootpath
+                .AppendPathSegment("/api/HospitalDepartment/list")
+                .WithOAuthBearerToken(await getToken())
+                .PostJsonAsync(new PagerQuery<HospitalDepartmentListQueryModel> { })
+                .ReceiveJson<OkMessage<PagerResult<HospitalDepartmentListApiModel>>>();
+            Assert.AreEqual(200, message.Code);
+        }
+        [TestMethod]
+        public async Task HospitalDepartment_AddAndDelete_ReturnIntAsync()
+        {
+            var hospital = await _rootpath
+                .AppendPathSegment("/api/HospitalDepartment/add")
+                .WithOAuthBearerToken(await getToken())
+                .PostJsonAsync(new HospitalDepartmentCreateApiModel
+                {
+                    Name = "q",
+                    HospitalId = 1,
+                })
+                .ReceiveJson<OkMessage<foundation.ef5.poco.HospitalDepartment>>();
+            var message = await _rootpath
+                .AppendPathSegment($"/api/HospitalDepartment/{hospital.Data.Id}/delete")
+                .WithOAuthBearerToken(await getToken())
+                .GetJsonAsync<OkMessage<int>>();
+            Assert.AreEqual(200, message.Code);
+            Assert.IsTrue(message.Data > 0);
+        }
+        [TestMethod]
+        public async Task HospitalDepartment_Update_ReturnIntAsync()
+        {
+            var message = await _rootpath
+                .AppendPathSegment("/api/HospitalDepartment/1/update")
+                .WithOAuthBearerToken(await getToken())
+                .PostJsonAsync(new HospitalDepartmentUpdateApiModel
+                {
+                    Id = 6,
+                    Name = "q",
+                })
+                .ReceiveJson<OkMessage<int>>();
+            Assert.AreEqual(200, message.Code);
+            Assert.IsTrue(message.Data > 0);
+        }
+
+
+    }
+}
