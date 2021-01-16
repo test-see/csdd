a22fdeb9a6e923d5fadb65dc9381e45980bd4f24
wancheo <wang406776@126.com>
2021-01-16 16:56:19 +0800

    add goods mapping model

---
+++ src/irespository/client/goods/model/ClientMappingGoodsListApiModel.cs
@@ -0,0 +1,10 @@
+﻿namespace irespository.client.goods.model
+{
+    public class ClientMappingGoodsListApiModel
+    {
+        public int ClientGoodsId { get; set; }
+        public int HospitalClientGoodsId { get; set; }
+        public int ClientQty { get; set; }
+        public int HospitalQty { get; set; }
+    }
+}
