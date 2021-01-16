132201ca07a4675e287cb8ce746bf43680787fba
wancheo <wang406776@126.com>
2021-01-16 17:00:21 +0800

    ee

---
+++ src/irespository/client/goods/model/ClientGoodsValueModel.cs
@@ -0,0 +1,14 @@
+﻿using irespository.client.profile.model;
+
+namespace irespository.client.goods.model
+{
+    public class ClientGoodsValueModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Spec { get; set; }
+        public string UnitPurchase { get; set; }
+        public string Producer { get; set; }
+        public ClientValueModel Client { get; set; }
+    }
+}
--- src/irespository/client/goods/model/ClientMappingGoodsListApiModel.cs
+++ src/irespository/client/goods/model/ClientMappingGoodsListApiModel.cs
@@ -1,10 +1,14 @@
-﻿namespace irespository.client.goods.model
+﻿using System;
+
+namespace irespository.client.goods.model
 {
     public class ClientMappingGoodsListApiModel
     {
-        public int ClientGoodsId { get; set; }
+        public ClientGoodsValueModel Goods { get; set; }
         public int HospitalClientGoodsId { get; set; }
         public int ClientQty { get; set; }
         public int HospitalQty { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string CreateUserName { get; set; }
     }
 }
