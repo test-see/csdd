d47b0acfa15548849dd2c5a3ed594146c631ddac
wancheo <wang406776@126.com>
2021-01-16 17:38:12 +0800

    done

--- src/irespository/client/goods/model/ClientMappingGoodsListApiModel.cs
+++ src/irespository/client/goods/model/ClientMappingGoodsListApiModel.cs
@@ -8,7 +8,5 @@ namespace irespository.client.goods.model
         public HospitalGoodsValueModel HospitalGoods { get; set; }
         public int ClientQty { get; set; }
         public int HospitalQty { get; set; }
-        public DateTime CreateTime { get; set; }
-        public string CreateUserName { get; set; }
     }
 }
--- src/respository/client/ClientGoodsRespository.cs
+++ src/respository/client/ClientGoodsRespository.cs
@@ -5,6 +5,8 @@ using irespository.client;
 using irespository.client.goods.model;
 using irespository.client.model;
 using irespository.client.profile.model;
+using irespository.hospital.goods.model;
+using irespository.hospital.profile.model;
 using System;
 using System.Linq;
 
@@ -139,12 +141,32 @@ namespace respository.client
                           UnitPurchase = r.UnitPurchase,
                           CreateUserName = u.Username,
                           IsActive = r.IsActive,
-                          
+
                       };
 
             var profile = sql.FirstOrDefault();
-            //profile.Mappings = 
-
+            profile.Mappings = (from m in _context.ClientMappingGoods
+                                join g in _context.HospitalGoods on m.HospitalGoodsId equals g.Id
+                                join h in _context.Hospital on g.HospitalId equals h.Id
+                                select new ClientMappingGoodsListApiModel
+                                {
+                                    ClientQty = m.ClientQty,
+                                    HospitalQty = m.HospitalQty,
+                                    HospitalGoods = new HospitalGoodsValueModel
+                                    {
+                                        Id = g.Id,
+                                        Name = g.Name,
+                                        Producer = g.Producer,
+                                        Spec = g.Spec,
+                                        UnitPurchase = g.UnitPurchase,
+                                        Hospital = new HospitalValueModel
+                                        {
+                                            Id = h.Id,
+                                            Name = h.Name,
+                                            Remark = h.Remark,
+                                        },
+                                    }
+                                }).ToList();
             return profile;
         }
     }
