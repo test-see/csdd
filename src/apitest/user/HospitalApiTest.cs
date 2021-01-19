6d8e968fbc9f090783b62724091bf037dce084b5
wancheo <wang406776@126.com>
2021-01-20 00:07:50 +0800

    Create HospitalApiTest.cs

---
+++ src/apitest/user/HospitalApiTest.cs
@@ -0,0 +1,40 @@
+﻿using apitest.shared;
+using Flurl;
+using Flurl.Http;
+using foundation.config;
+using irespository.user.hospital.model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace apitest.user
+{
+    [TestClass]
+    public class UserHospitalApiTest : BaseApiTest
+    {
+        [TestMethod]
+        public async Task UserHospital_Post_ReturnListAsync()
+        {
+            var message = await _rootpath
+                .AppendPathSegment("/api/UserHospital/list")
+                .WithOAuthBearerToken(await getToken())
+                .PostJsonAsync(new PagerQuery<UserHospitalListQueryModel> { })
+                .ReceiveJson<OkMessage<PagerResult<UserHospitalListApiModel>>>();
+            Assert.AreEqual(200, message.Code);
+        }
+        [TestMethod]
+        public async Task UserHospital_AddAndDelete_ReturnIntAsync()
+        {
+            var UserHospital = await _rootpath
+                .AppendPathSegment("/api/UserHospital/add")
+                .WithOAuthBearerToken(await getToken())
+                .PostJsonAsync(new UserHospitalCreateApiModel { Name = "q", HospitalDepartmentId = 1, UserId = 1 })
+                .ReceiveJson<OkMessage<foundation.ef5.poco.UserHospital>>();
+            var message = await _rootpath
+                .AppendPathSegment($"/api/UserHospital/{UserHospital.Data.Id}/delete")
+                .WithOAuthBearerToken(await getToken())
+                .GetJsonAsync<OkMessage<int>>();
+            Assert.AreEqual(200, message.Code);
+            Assert.IsTrue(message.Data > 0);
+        }
+    }
+}
