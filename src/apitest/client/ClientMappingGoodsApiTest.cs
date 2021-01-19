0b6fc918bd5833c320aa6b3f00fe87b20e02aebf
wancheo <wang406776@126.com>
2021-01-19 23:08:27 +0800

    Create ClientMappingGoodsApiTest.cs

---
+++ src/apitest/client/ClientMappingGoodsApiTest.cs
@@ -0,0 +1,36 @@
+﻿using apitest.shared;
+using Flurl;
+using Flurl.Http;
+using foundation.config;
+using irespository.client.goods.model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace apitest.client
+{
+    [TestClass]
+    public class ClientMappingGoodsApiTest : BaseApiTest
+    {
+        [TestMethod]
+        public async Task ClientMappingGoods_AddAndDelete_ReturnIntAsync()
+        {
+            var Client = await _rootpath
+                .AppendPathSegment("/api/ClientMappingGoods/add")
+                .WithOAuthBearerToken(await getToken())
+                .PostJsonAsync(new ClientMappingGoodsCreateApiModel
+                {
+                    ClientGoodsId = 1,
+                    ClientQty = 1,
+                    HospitalGoodsId = 1,
+                    HospitalQty = 1
+                })
+                .ReceiveJson<OkMessage<foundation.ef5.poco.ClientMappingGoods>>();
+            var message = await _rootpath
+                .AppendPathSegment($"/api/ClientMappingGoods/{Client.Data.Id}/delete")
+                .WithOAuthBearerToken(await getToken())
+                .GetJsonAsync<OkMessage<int>>();
+            Assert.AreEqual(200, message.Code);
+            Assert.IsTrue(message.Data > 0);
+        }
+    }
+}
