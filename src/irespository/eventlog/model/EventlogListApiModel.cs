75ad69139002a08bd5e76a23de4e563da3044b9b
wancheo <wang406776@126.com>
2021-01-20 16:35:05 +0800

    Update EventlogListApiModel.cs

--- src/irespository/eventlog/model/EventlogListApiModel.cs
+++ src/irespository/eventlog/model/EventlogListApiModel.cs
@@ -5,7 +5,6 @@ namespace irespository.sys.model
     public class EventlogListApiModel
     {
         public int Id { get; set; }
-        public string Group { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime CreateTime { get; set; }
