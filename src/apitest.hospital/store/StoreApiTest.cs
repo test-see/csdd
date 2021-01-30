9eb38642cc8262a7ae5abae3ac7771dc63a78579
wancheo <wang406776@126.com>
2021-01-30 14:55:28 +0800

    add api test

---
+++ src/apitest.hospital/store/StoreApiTest.cs
@@ -0,0 +1,57 @@
+﻿using apitest.shared;
+using Flurl;
+using Flurl.Http;
+using foundation.config;
+using foundation.ef5.poco;
+using irespository.store.model;
+using irespository.store.profile.model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apitest.hospital.store
+{
+    [TestClass]
+    public class StoreApiTest : BaseApiTest
+    {
+        [TestMethod]
+        public async Task Store_List_ReturnListAsync()
+        {
+            var message = await _rootpath
+                .AppendPathSegment("/api/Store/list")
+                .WithOAuthBearerToken(await getToken())
+                .PostJsonAsync(new PagerQuery<StoreListQueryModel> { })
+                .ReceiveJson<OkMessage<PagerResult<StoreListApiModel>>>();
+            Assert.AreEqual(200, message.Code);
+            Assert.IsTrue(message.Data.Total > 0);
+        }
+        [TestMethod]
+        public async Task Store_Add_ReturnIntAsync()
+        {
+            var message = await _rootpath
+                .AppendPathSegment("/api/Store/add")
+                .WithOAuthBearerToken(await getToken())
+                .PostJsonAsync(new CustomizeStoreChangeApiModel
+                {
+                    HospitalGoodsId = 1,
+                    ChangeQty = 1,
+                    ChangeTypeId = 1
+                })
+                .ReceiveJson<OkMessage<foundation.ef5.poco.Store>>();
+            Assert.AreEqual(200, message.Code);
+        }
+        [TestMethod]
+        public async Task HospitalDepartment_Type_ReturnListAsync()
+        {
+            var message = await _rootpath
+                .AppendPathSegment("/api/Store/changetype")
+                .WithOAuthBearerToken(await getToken())
+                .GetJsonAsync<OkMessage<IEnumerable<DataStoreChangeType>>>();
+            Assert.AreEqual(200, message.Code);
+            Assert.IsTrue(message.Data.Any());
+        }
+    }
+}
