d35ec69ac7d33006b94a41e82f485a275cc0a35e
wancheo <wang406776@126.com>
2021-02-02 23:38:29 +0800

    add test

---
+++ src/apitest.hospital/prescription/PrescriptionApiTest.cs
@@ -0,0 +1,44 @@
+﻿using apitest.shared;
+using Flurl;
+using Flurl.Http;
+using foundation.config;
+using irespository.prescription.model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apitest.hospital.prescription
+{
+    [TestClass]
+    public class PrescriptionApiTest : BaseApiTest
+    {
+        [TestMethod]
+        public async Task Prescription_List_ReturnListAsync()
+        {
+            var message = await _rootpath
+                .AppendPathSegment("/api/Prescription/list")
+                .WithOAuthBearerToken(await getToken())
+                .PostJsonAsync(new PagerQuery<PrescriptionListQueryModel> { })
+                .ReceiveJson<OkMessage<PagerResult<PrescriptionListApiModel>>>();
+            Assert.AreEqual(200, message.Code);
+            Assert.IsTrue(message.Data.Total > 0);
+        }
+        [TestMethod]
+        public async Task Prescription_Add_ReturnIntAsync()
+        {
+            var message = await _rootpath
+                .AppendPathSegment("/api/Prescription/add")
+                .WithOAuthBearerToken(await getToken())
+                .PostJsonAsync(new PrescriptionCreateApiModel
+                {
+                    Cardno = "11",
+                    HospitalGoods = new List<KeyValuePair<int, int>> { new KeyValuePair<int, int>(1, 1) },
+                })
+                .ReceiveJson<OkMessage<foundation.ef5.poco.Prescription>>();
+            Assert.AreEqual(200, message.Code);
+        }
+    }
+}
