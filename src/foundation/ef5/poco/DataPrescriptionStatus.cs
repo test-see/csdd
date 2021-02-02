c222af0feb6c32e11df9f89c071e092265f082f1
wancheo <wang406776@126.com>
2021-02-02 19:03:17 +0800

    add DataPrescriptionStatus

---
+++ src/foundation/ef5/poco/DataPrescriptionStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace foundation.ef5.poco
+{
+    [Table("data_prescription_status")]
+    public class DataPrescriptionStatus
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+    }
+}
