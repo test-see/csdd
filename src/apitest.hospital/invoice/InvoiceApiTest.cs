501977755678a8f04157cfbf142e519bdbfe7395
wancheo <wang406776@126.com>
2021-02-16 15:37:40 +0800

    add test

---
+++ src/apitest.hospital/invoice/InvoiceApiTest.cs
@@ -0,0 +1,81 @@
+﻿using apitest.shared;
+using Flurl;
+using Flurl.Http;
+using foundation.config;
+using irespository.invoice.model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace apitest.Invoice
+{
+    [TestClass]
+    public class InvoiceTest : BaseApiTest
+    {
+        [TestMethod]
+        public async Task Invoice_Post_ReturnListAsync()
+        {
+            var message = await _rootpath
+                .AppendPathSegment("/api/Invoice/list")
+                .WithOAuthBearerToken(await getToken())
+                .PostJsonAsync(new PagerQuery<InvoiceListQueryModel> { })
+                .ReceiveJson<OkMessage<PagerResult<InvoiceListApiModel>>>();
+            Assert.AreEqual(200, message.Code);
+            Assert.IsTrue(message.Data.Total > 0);
+        }
+        [TestMethod]
+        public async Task Invoice_AddAndDelete_ReturnIntAsync()
+        {
+            var hospital = await _rootpath
+                .AppendPathSegment("/api/Invoice/add")
+                .WithOAuthBearerToken(await getToken())
+                .PostJsonAsync(new InvoiceCreateApiModel
+                {
+                    Name = "1",
+                    Remark = "2",
+                    EndDate = DateTime.Now,
+                    StartDate = DateTime.Now,
+                })
+                .ReceiveJson<OkMessage<foundation.ef5.poco.Invoice>>();
+            var message = await _rootpath
+                .AppendPathSegment($"/api/Invoice/{hospital.Data.Id}/delete")
+                .WithOAuthBearerToken(await getToken())
+                .GetJsonAsync<OkMessage<int>>();
+            Assert.AreEqual(200, message.Code);
+            Assert.IsTrue(message.Data > 0);
+        }
+        [TestMethod]
+        public async Task Invoice_Update_ReturnIntAsync()
+        {
+            var message = await _rootpath
+                .AppendPathSegment("/api/Invoice/1/update")
+                .WithOAuthBearerToken(await getToken())
+                .PostJsonAsync(new InvoiceUpdateApiModel { Name = "1", Remark = "2", Id = 1 })
+                .ReceiveJson<OkMessage<int>>();
+            Assert.AreEqual(200, message.Code);
+            Assert.IsTrue(message.Data > 0);
+        }
+
+
+        [TestMethod]
+        public async Task Invoice_Index_ReturnListAsync()
+        {
+            var message = await _rootpath
+                .AppendPathSegment("/api/Invoice/1/index")
+                .WithOAuthBearerToken(await getToken())
+                .GetJsonAsync<OkMessage<InvoiceIndexApiModel>>();
+            Assert.AreEqual(200, message.Code);
+            Assert.IsTrue(message.Data != null);
+        }
+
+        [TestMethod]
+        public async Task Invoice_Submit_ReturnListAsync()
+        {
+            var message = await _rootpath
+                .AppendPathSegment("/api/Invoice/1/submit")
+                .WithOAuthBearerToken(await getToken())
+                .GetJsonAsync<OkMessage<int>>();
+            Assert.AreEqual(200, message.Code);
+        }
+    }
+}
