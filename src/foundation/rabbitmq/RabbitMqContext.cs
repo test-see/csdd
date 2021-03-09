9eea9a678217593a3cd6e27d57cdaaf9e91319a9
wancheo <wang406776@126.com>
2021-03-09 18:22:26 +0800

    ==

---
+++ src/foundation/rabbitmq/RabbitMqContext.cs
@@ -0,0 +1,28 @@
+﻿using foundation.config;
+using RabbitMQ.Client;
+
+namespace foundation.rabbitmq
+{
+    public class RabbitMqContext
+    {
+        protected IConnection _connection;
+        private readonly RabbitMqConfig _config;
+        public RabbitMqContext(RabbitMqConfig config)
+        {
+            _config = config;
+        }
+        protected IModel GetChannel()
+        {
+            if (_connection == null)
+            {
+                var factory = new ConnectionFactory();
+                factory.Port = _config.Port;
+                factory.UserName = _config.UserName;
+                factory.VirtualHost = _config.HostName;
+                factory.Password = _config.Password;
+                _connection = factory.CreateConnection();
+            }
+            return _connection.CreateModel();
+        }
+    }
+}
