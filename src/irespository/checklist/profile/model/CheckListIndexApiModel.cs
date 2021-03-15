b5a73361c9479c3f93ae827411c6242f8999cc15
wancheo <wang406776@126.com>
2021-03-15 23:57:20 +0800

    77

--- src/irespository/checklist/profile/model/CheckListIndexApiModel.cs
+++ src/irespository/checklist/profile/model/CheckListIndexApiModel.cs
@@ -11,5 +11,6 @@ namespace irespository.checklist.model
         public string Remark { get; set; }
         public DateTime CreateTime { get; set; }
         public string CreateUserName { get; set; }
+        public int Status { get; set; }
     }
 }
