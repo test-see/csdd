8f0ebf7d09474ff70898e5be93e1df6585d849b0
chao.wang <chao.wang@starlight-sms.com>
2021-04-12 11:52:51 +0800

    clean code

--- src/foundation/servicecollection/ServiceCollectionExtension.cs
+++ src/foundation/servicecollection/ServiceCollectionExtension.cs
@@ -31,7 +31,7 @@ namespace foundation.servicecollection
             services.AddSingleton(RabbitHutch.CreateBus("host=localhost"));
 
             var mediaBuilder = new MediatorBuilder();
-            mediaBuilder.RegisterHandlers(Assembly.Load("db"), Assembly.Load("bff"));
+            mediaBuilder.RegisterHandlers(Assembly.Load("pipe"));
             services.RegisterMediator(mediaBuilder);
         }
     }
