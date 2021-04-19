4903ca7a59fa8b67c039e3d8be960d5b1f1339c1
chao.wang <chao.wang@starlight-sms.com>
2021-04-19 22:22:14 +0800

    Update ListDepartmentTypeRequestHandler.cs

--- src/storage/data/ListDepartmentTypeRequestHandler.cs
+++ src/storage/data/ListDepartmentTypeRequestHandler.cs
@@ -1,25 +1,27 @@
-﻿using foundation.ef5;
+﻿using foundation.config;
+using foundation.ef5;
 using foundation.ef5.poco;
 using foundation.mediator;
 using Mediator.Net.Context;
 using Mediator.Net.Contracts;
 using Microsoft.EntityFrameworkCore;
 using storage.data.carrier;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace mediator.client.profile
 {
-    public class ListDepartmentTypeRequestHandler : IRequestHandler<ListDepartmentTypeRequest, ListResponse<DataDepartmentType>>
+    public class ListDepartmentTypeRequestHandler : IRequestHandler<ListDepartmentTypeRequest, ListResponse<IdNameValueModel>>
     {
         private readonly DefaultDbContext _context;
         public ListDepartmentTypeRequestHandler(DefaultDbContext context)
         {
             _context = context;
         }
-        public async Task<ListResponse<DataDepartmentType>> Handle(IReceiveContext<ListDepartmentTypeRequest> context, CancellationToken cancellationToken)
+        public async Task<ListResponse<IdNameValueModel>> Handle(IReceiveContext<ListDepartmentTypeRequest> context, CancellationToken cancellationToken)
         {
-            var data = await  _context.DataDepartmentType.ToListAsync();
+            var data = await  _context.DataDepartmentType.Select(x => new IdNameValueModel { Id = x.Id, Name = x.Name }).ToListAsync();
             return data.ToResponse();
         }
     }
